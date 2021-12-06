6d49b048729a1b4417914cc6364f138f2a0b322d
Karthik Mohanasundaram <connect2kms@outlook.com>
2021-12-06 11:24:48 -0500

    Replaced noPropertyAccessFromIndexSignature flag

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { CardFormComponent } from './card-form/card-form.component';
 
