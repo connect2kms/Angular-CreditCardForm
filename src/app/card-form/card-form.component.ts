528d4ceebfd7eff4a83d6cb6a644935e569d0187
Karthik Mohanasundaram <connect2kms@outlook.com>
2021-12-06 18:58:04 -0500

    Added Rest button

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -38,5 +38,9 @@ export class CardFormComponent implements OnInit {
   onSubmit() {
     console.log('Form was submitted');
   }
+
+  onReset() {
+    this.cardForm.reset();
+  }
   ngOnInit() {}
 }
